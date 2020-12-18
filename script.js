64c63e1dd412689cf1f1f90f71aafc63dd83a0d0
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-12-17 21:44:06 -0600

    refactor: Audio url logic to be only strings

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-const shaker = new Audio(
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Shaker.wav"
-);
-const clap = new Audio(
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Clap.wav"
-);
-const conga = new Audio(
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Conga.wav"
-);
-const hihat = new Audio(
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/HiHat.wav"
-);
-const stick = new Audio(
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Stick.wav"
-);
-const tom = new Audio(
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Tom.wav"
-);
+const urls = [
+  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Shaker.wav",
+  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Clap.wav",
+  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Conga.wav",
+  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/HiHat.wav",
+  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Stick.wav",
+  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Tom.wav",
+];
+
+const audio = new Audio();
 
 const buttons = document.querySelectorAll(".btn");
 
 console.log(buttons);
 
 const setupAudios = () => {
-  buttons[0].addEventListener("click", () => shaker.play());
-  buttons[1].addEventListener("click", () => clap.play());
-  buttons[2].addEventListener("click", () => conga.play());
-  buttons[3].addEventListener("click", () => hihat.play());
-  buttons[4].addEventListener("click", () => stick.play());
-  buttons[5].addEventListener("click", () => tom.play());
+  buttons[0].addEventListener("click", () => {
+    audio.src = urls[0];
+    audio.play();
+  });
+  buttons[1].addEventListener("click", () => {
+    audio.src = urls[1];
+    audio.play();
+  });
+  buttons[2].addEventListener("click", () => {
+    audio.src = urls[2];
+    audio.play();
+  });
+  buttons[3].addEventListener("click", () => {
+    audio.src = urls[3];
+    audio.play();
+  });
+  buttons[4].addEventListener("click", () => {
+    audio.src = urls[4];
+    audio.play();
+  });
+  buttons[5].addEventListener("click", () => {
+    audio.src = urls[5];
+    audio.play();
+  });
 };
 
 setupAudios();
