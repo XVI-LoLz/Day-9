39236468b60b3ddd0076023b2d0de4f7f790afe2
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-12-17 22:05:16 -0600

    refactor: Add flexibility to audio console
    Create HTML elements dynamically and append to a selected parent when you call setupAudios
    
    setupAudios(audiosArray, parentElement);

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-const urls = [
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Shaker.wav",
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Clap.wav",
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Conga.wav",
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/HiHat.wav",
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Stick.wav",
-  "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Tom.wav",
+const audios = [
+  {
+    name: "shaker",
+    url:
+      "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Shaker.wav",
+  },
+  {
+    name: "clap",
+    url:
+      "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Clap.wav",
+  },
+  {
+    name: "conga",
+    url:
+      "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Conga.wav",
+  },
+  {
+    name: "hihat",
+    url:
+      "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/HiHat.wav",
+  },
+  {
+    name: "stick",
+    url:
+      "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Stick.wav",
+  },
+  {
+    name: "tom",
+    url:
+      "https://z16th-bucket.s3-us-west-1.amazonaws.com/fcc-drum-machine/Tom.wav",
+  },
 ];
 
-const buttons = document.querySelectorAll(".btn");
+const container = document.querySelector(".container");
+const mainButton = document.querySelector(".main-button");
 
-const setupAudios = () => {
-  buttons.forEach((button, i) =>
-    button.addEventListener("click", () => {
-      const audio = new Audio();
-      audio.src = urls[i];
-      audio.play();
-    })
-  );
+const setupAudios = (audiosArr, parent) => {
+  audiosArr.forEach((audio) => {
+    const element = document.createElement("button");
+    element.classList.add("btn");
+    element.innerHTML = audio.name;
+
+    const a = new Audio();
+    a.src = audio.url;
+    element.addEventListener("click", () => a.play());
+    parent.appendChild(element);
+  });
 };
 
-setupAudios();
+setupAudios(audios, mainButton);
